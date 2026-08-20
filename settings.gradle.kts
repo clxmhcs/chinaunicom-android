888b9a3ab7a2e909beac4d4eb9448c70090e6e95
clxmhcs <109405345+clxmhcs@users.noreply.github.com>
2026-08-20 18:55:21 +0800

    Android-M4-G0.5-B: fix Gradle repository import

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,5 @@
+import org.gradle.api.initialization.resolve.RepositoriesMode
+
 pluginManagement {
     repositories {
         google()
@@ -6,7 +8,7 @@ pluginManagement {
     }
 }
 
- dependencyResolutionManagement {
+dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
         google()
