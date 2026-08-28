00694c2e1f56021a22ed22a8699088e134f0501e
clxmhcs <109405345+clxmhcs@users.noreply.github.com>
2026-08-28 20:10:42 +0800

    M14-A: link capture module into app

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
         minSdk = 30
         targetSdk = 36
         versionCode = 1
-        versionName = "0.9.0-m9h1r1-i1r2-m10a1r2-m12b1-m13a1-m13b1"
+        versionName = "0.9.0-m9h1r1-i1r2-m10a1r2-m12b1-m13a1-m13b1-m14a1"
     }
 
     buildTypes {
@@ -60,6 +60,7 @@ dependencies {
     implementation(project(":data:tariffzone"))
     implementation(project(":data:videoring"))
     implementation(project(":widget"))
+    implementation(project(":capture"))
 
     implementation(platform(libs.androidx.compose.bom))
     implementation(libs.androidx.activity.compose)
