dc86369b1556e49fd72a0faa4385e4c0e9c25db5
clxmhcs <109405345+clxmhcs@users.noreply.github.com>
2026-08-28 16:40:28 +0800

    M13-A bump app version marker

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
         minSdk = 30
         targetSdk = 36
         versionCode = 1
-        versionName = "0.9.0-m9h1r1-i1r2-m10a1r2-m12b1"
+        versionName = "0.9.0-m9h1r1-i1r2-m10a1r2-m12b1-m13a1"
     }
 
     buildTypes {
